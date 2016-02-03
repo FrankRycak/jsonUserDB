08914a1aee95ca7b093ec779b0ae07179c4f8922
FrankRycak <frank.rycak@icloud.com>
2016-02-02 23:44:32 -0500

    updated error

--- runner.rb
+++ runner.rb
@@ -1,11 +1,9 @@
 require 'pp'
 require_relative 'User'
 
-<<<<<<< HEAD
+
 user = User.new 'kingKong@empirestate.com', 'Moe'
-=======
-user = User.new 'JohnDoe@example.com', 'John'
->>>>>>> 9511488b08d5e69782325f1c53bb3c7af821e604
+
 
 pp user
 
